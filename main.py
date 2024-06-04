98518fa36e9e99b6c65ce7b55e4885306583d884
Cole Baril <colewbaril@outlook.com>
2024-06-04 17:45:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from func import *  # Import all functions from func.py
+
+if __name__ == "__main__":
+    # Replace post_date with the logic or function you want to execute
+    print("Executing main script logic...")
+    # Tweet
+    message = "Hello from GitHub Actions!"
+    tweet(message)

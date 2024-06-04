8613b952de1d8991dfec342ef2fa7d6c880fd8ec
Cole Baril <colewbaril@outlook.com>
2024-06-04 17:49:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from func import *  # Import all functions from func.py
+from func import tweet  # Import the tweet function from func.py
 
 if __name__ == "__main__":
-    # Replace post_date with the logic or function you want to execute
+    # Logic to execute the tweet function
     print("Executing main script logic...")
-    # Tweet
     message = "Hello from GitHub Actions!"
     tweet(message)

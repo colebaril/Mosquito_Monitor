8ecc141c7af8e3e9dffebce974751b505bf94c72
Cole Baril <colewbaril@outlook.com>
2024-06-04 17:58:56 -0500

    Update func.py

--- func.py
+++ func.py
@@ -18,5 +18,5 @@ def create_api():
 
 def tweet(message):
     api = create_api()
-    tweepy.Client.create_tweet(message)
+    tweepy.Client.create_tweet(text=message)
     print("Tweeted:", message)

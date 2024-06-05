8483d050e19232763d53a98da47f8f510fe04cb6
Cole Baril <colewbaril@outlook.com>
2024-06-04 22:37:54 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -12,6 +12,9 @@ access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
 client = tweepy.Client(consumer_key=consumer_key,consumer_secret=consumer_secret,access_token=access_token,access_token_secret=access_token_secret)
 
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
 if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
     raise ValueError("Twitter API credentials are not set properly")
 

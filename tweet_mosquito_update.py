e747464bfba5f0351f32395e20f97b5e6226e70f
Cole Baril <colewbaril@outlook.com>
2024-06-04 22:04:31 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -1,5 +1,8 @@
 import tweepy
 import os
+import requests
+from PIL import Image
+from io import BytesIO
 
 
 consumer_key = os.getenv('CONSUMER_TOKEN')
@@ -12,15 +15,21 @@ client = tweepy.Client(consumer_key=consumer_key,consumer_secret=consumer_secret
 if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
     raise ValueError("Twitter API credentials are not set properly")
 
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
+# URL of the image in the GitHub repository
+image_url = 'https://raw.githubusercontent.com/colebaril/Mosquito_Monitor/main/wpg_mosquito_map_tmp.png'
+
+# Send a GET request to the image URL
+response = requests.get(image_url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Open the image from the response content
+    image = Image.open(BytesIO(response.content))
+else:
+    print(f"Failed to retrieve the image. Status code: {response.status_code}")
     
-# api = tweepy.Client(auth)
-#    return api
-
-
-
 # Create a tweet
-message="Hello from GitHub Actions"
-client.create_tweet(text=message)
+message="Hello from GitHub Actions. This is a test."
+media_id = image
+client.create_tweet(media_ids = [media_id], text=message)
 print("Tweeted!")

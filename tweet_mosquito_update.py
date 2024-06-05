e66284e252095fedf42a7843bca2ffd9552fe0a3
Cole Baril <colewbaril@outlook.com>
2024-06-04 22:50:55 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -1,6 +1,7 @@
 import tweepy
 import os
 import requests
+from datetime import datetime
 
 
 consumer_key = os.getenv('CONSUMER_TOKEN')
@@ -52,8 +53,10 @@ else:
 media_path = local_image_path
 media = client_v1.media_upload(filename=media_path)
 media_id = media.media_id
-    
+
+# Get the current date
+    current_date = datetime.now().strftime('%Y-%m-%d')
 # Create a tweet
-message="Hello from GitHub Actions. This is a test."
+message = f"City of Winnipeg mosquito trap counts have been updated as of {current_date}. See a detailed update here: https://github.com/colebaril/Mosquito_Monitor"
 client_v2.create_tweet(media_ids=[media_id], text=message)
 print("Tweeted!")

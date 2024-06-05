4b1933d2fa9924a2b43d1eeb1912f2795d86aabd
Cole Baril <colewbaril@outlook.com>
2024-06-04 22:45:41 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -10,30 +10,30 @@ consumer_secret = os.getenv('CONSUMER_TOKEN_SECRET')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-def get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret) -> tweepy.API:
+def get_twitter_conn_v1(consumer_key, consumer_secret, access_token, access_token_secret) -> tweepy.API:
     """Get twitter conn 1.1"""
 
-    auth = tweepy.OAuth1UserHandler(api_key, api_secret)
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
     auth.set_access_token(
         access_token,
         access_token_secret,
     )
     return tweepy.API(auth)
 
-def get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret) -> tweepy.Client:
+def get_twitter_conn_v2(consumer_key, consumer_secret, access_token, access_token_secret) -> tweepy.Client:
     """Get twitter conn 2.0"""
 
     client = tweepy.Client(
-        consumer_key=api_key,
-        consumer_secret=api_secret,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
         access_token=access_token,
         access_token_secret=access_token_secret,
     )
 
     return client
 
-client_v1 = get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret)
-client_v2 = get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret)
+client_v1 = get_twitter_conn_v1(consumer_key, consumer_secret, access_token, access_token_secret)
+client_v2 = get_twitter_conn_v2(consumer_key, consumer_secret, access_token, access_token_secret)
 
 if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
     raise ValueError("Twitter API credentials are not set properly")

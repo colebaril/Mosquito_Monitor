3dcdb420350396338bcf74c5cb4172ea2abef739
Cole Baril <colewbaril@outlook.com>
2024-06-04 20:59:16 -0500

    Update func.py

--- func.py
+++ func.py
@@ -1,22 +1,26 @@
 import tweepy
 import os
 
-def create_api():
-    consumer_key = os.getenv('CONSUMER_TOKEN')
-    consumer_secret = os.getenv('CONSUMER_TOKEN_SECRET')
-    access_token = os.getenv('ACCESS_TOKEN')
-    access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+consumer_key = os.getenv('CONSUMER_TOKEN')
+consumer_secret = os.getenv('CONSUMER_TOKEN_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+client = tweepy.Client(consumer_key=consumer_key,consumer_secret=consumer_secret,access_token=access_token,access_token_secret=access_token_secret)
 
 if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
     raise ValueError("Twitter API credentials are not set properly")
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
     
-    api = tweepy.Client(auth)
-    return api
+# api = tweepy.Client(auth)
+#    return api
 
-def tweet(message):
-    api = create_api()
-    api.create_tweet(text=message)
-    print("Tweeted:", message)
+
+
+# Create a tweet
+
+client.create_tweet(text=message)
+print("Tweeted!")

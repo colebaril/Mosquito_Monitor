083dd6bcc2618df0faa3194411f1f2269958b9a5
Cole Baril <colewbaril@outlook.com>
2024-06-04 22:29:42 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -15,22 +15,14 @@ client = tweepy.Client(consumer_key=consumer_key,consumer_secret=consumer_secret
 if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
     raise ValueError("Twitter API credentials are not set properly")
 
-# URL of the image in the GitHub repository
-image_url = 'https://raw.githubusercontent.com/colebaril/Mosquito_Monitor/main/wpg_mosquito_map_tmp.png'
+ # Upload the image
+    media = api.media_upload('https://raw.githubusercontent.com/colebaril/Mosquito_Monitor/main/wpg_mosquito_map_tmp.png')
 
-# Send a GET request to the image URL
-response = requests.get(image_url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Open the image from the response content
-    image = Image.open(BytesIO(response.content))
-    
-else:
-    print(f"Failed to retrieve the image. Status code: {response.status_code}")
+    # Get the media_id
+    media_id = media.media_id_string
     
 # Create a tweet
 message="Hello from GitHub Actions. This is a test."
-media_id = [image]
+media_id = [media_id]
 client.create_tweet(media_ids=media_id, text=message)
 print("Tweeted!")

a206538a6172ac063d3bb10b68bd20308b4cf4ba
Cole Baril <colewbaril@outlook.com>
2025-05-09 19:43:41 -0500

    Update update_wpg_figure.R

--- update_wpg_figure.R
+++ update_wpg_figure.R
@@ -9,7 +9,9 @@ library(readr)
 
 # Wait 3 minutes before running. Weird bug occurred where the previous data is used if the figure update script starts too fast. 
 
-#Sys.sleep(180)
+Sys.setenv(TZ = "America/Winnipeg")
+
+Sys.sleep(180)
 
 master_data <- read_csv(url("https://github.com/colebaril/Mosquito_Monitor/blob/main/mosquito_data.csv?raw=TRUE"))
 

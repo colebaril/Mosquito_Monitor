865028e3803a30786213ec5fef7a761ce688620e
Cole Baril <colewbaril@outlook.com>
2025-05-09 20:01:48 -0500

    Update tweet_mosquito_update.py

--- tweet_mosquito_update.py
+++ tweet_mosquito_update.py
@@ -61,6 +61,6 @@ media_id = media.media_id
 current_date = datetime.now().strftime('%Y-%m-%d')
 
 # Create a tweet
-message = f"City of Winnipeg mosquito trap counts have been updated as of {current_date}. See a detailed update here: https://shorturl.at/LkDqU. \n#Winnipeg #Mosquitoes #Mosquito #CityOfWinnipeg #HealthAlert"
+message = f"City of Winnipeg mosquito trap counts have been updated as of {current_date}. See a detailed update here: https://shorturl.at/MGzTL. \n#Winnipeg #Mosquitoes #Mosquito #CityOfWinnipeg #HealthAlert"
 client_v2.create_tweet(media_ids=[media_id], text=message)
 print("Tweeted!")

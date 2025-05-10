8b35e3014eea192f090a2195199245857b85f034
Cole Baril <colewbaril@outlook.com>
2025-05-09 20:06:07 -0500

    Update skeet_mosquito_update.py

--- skeet_mosquito_update.py
+++ skeet_mosquito_update.py
@@ -1,14 +1,16 @@
 from atproto import Client
 import os
-import requests
-import time
-from datetime import datetime
 
-
-
+# Get the Bluesky app password from the environment
 bsky_key = os.getenv('BSKY_KEY')
 
+# Ensure it's not None
+if not bsky_key:
+    raise ValueError("BSKY_KEY environment variable not set")
+
+# Initialize client and login
 client = Client()
 client.login('@mosquitomonitor.bsky.social', bsky_key)
 
-post = client.send_post('Hello world! I posted this via the Python SDK.')
+# Post your message
+client.send_post('Hello world! I posted this via GitHub Actions (test).')

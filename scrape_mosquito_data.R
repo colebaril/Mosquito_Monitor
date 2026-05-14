2c65cb97201d1f19a4de442ad184c2063d67fb0f
Cole Baril <colewbaril@outlook.com>
2026-05-14 18:51:57 -0500

    Update scrape_mosquito_data.R

--- scrape_mosquito_data.R
+++ scrape_mosquito_data.R
@@ -5,99 +5,84 @@ library(janitor)
 library(tidyr)
 library(lubridate)
 library(readr)
-library(httr)
 
-# Winnipeg mosquito trap dataset API endpoint
-api_url <- paste0(
-  "https://data.winnipeg.ca/resource/du7c-8488.csv?$limit=50000"
-)
+Sys.setenv(TZ = "America/Winnipeg")
 
-# Download latest data
-mosquito_data_latest <- read_csv(api_url,
-                        show_col_types = FALSE)
+url <- "https://legacy.winnipeg.ca/publicworks/insectcontrol/mosquitoes/trapcounts.stm"
 
+webpage <- read_html(url)
 
-# Old ----
+date_updated <- webpage |> 
+  html_table(fill = TRUE) %>% 
+  .[[1]] |> 
+  slice(1) |> select(2) |> pull() |> as.Date(., format = "%B %d, %Y")
 
-
-master_data_old <- read_csv(url("https://github.com/colebaril/Mosquito_Monitor/blob/main/mosquito_data.csv?raw=TRUE"))
-
-if(max(as.Date(master_data_old$date)) == max(as.Date(mosquito_data_latest$count_date))) {
+scrape_and_save_data <- function() {
   
-  message(paste0("Data is already up to date as of ", Sys.Date(), "."))
+  webpage <- read_html(url)
   
-  message(paste0("Data last updated: ", max(mosquito_data_latest$count_date), "."))
+  data <- webpage %>%
+    html_table(fill = TRUE) %>%
+    .[[2]]
   
-} else if(max(master_data_old$date) < max(mosquito_data_latest$count_date)) {
-  
-  message("New data detected. Running script...")
-
-trap_data_metro <- mosquito_data_latest |> 
-  select(1:3, starts_with("rural_")) |> 
-  mutate(across(where(is.character), ~na_if(., "no data"))) |> 
-  mutate(across(2:last_col(), ~as.numeric(.))) |> 
- 
-  pivot_longer(cols = matches("[a-z][a-z]") & -c("count_date", "city_wide_daily_average", "trap_days"),
-               names_to = "trap",
-               values_to = "number") |>  
-  rename(date = count_date) |>  
-  mutate(region = "Out of City Limits") |>  
-  mutate(number = as.numeric(number)) |> 
-  # distinct(trap, .keep_all = TRUE) |> 
-  mutate(region_name = case_when(str_detect(trap, "aa") ~ "Lilyfield",
-                            str_detect(trap, "bb") ~ "North Perimeter",
-                            str_detect(trap, "cc") ~ "West St. Paul",
-                            str_detect(trap, "dd") ~ "East St. Paul",
-                            str_detect(trap, "ee") ~ "Springfield",
-                            str_detect(trap, "ff") ~ "Ritchot",
-                            str_detect(trap, "gg") ~ "MacDonald",
-                            str_detect(trap, "hh") ~ "Headingly",
-                            str_detect(trap, "ii") ~ "Rosser",
-                            TRUE ~ NA)) |> 
-  mutate(trap = str_remove(trap, "rural_"))
-
-message("Metro data processed.")
-
-trap_data_wpg <- mosquito_data_latest |>  
-  select(-starts_with("rural_"), -north_west_average, -north_east_average, -south_east_average, -south_west_average) |> 
-  mutate(across(where(is.character), ~na_if(., "no data"))) |> 
-  mutate(across(2:last_col(), ~as.numeric(.))) |> 
-  
-  pivot_longer(cols = matches("[a-z][a-z]") & -c("count_date", "city_wide_daily_average", "trap_days"),
-               names_to = "trap",
-               values_to = "number") |>   
-  rename(date = count_date) |> 
-  mutate(
-    region = trap |>
-      str_remove("_\\d+$") |>
-      str_replace("north_west", "NW") |>
-      str_replace("north_east", "NE") |>
-      str_replace("south_west", "SW") |>
-      str_replace("south_east", "SE")
-  ) |> 
-  mutate(
-    region_name = trap |>
-      str_replace("north_west_", "NW") |>
-      str_replace("north_east_", "NE") |>
-      str_replace("south_west_", "SW") |>
-      str_replace("south_east_", "SE")
-  ) 
-
-message("City data processed.")
-
-
-master_data <- rbind(trap_data_wpg, trap_data_metro) 
-  
-message(paste0("Data has been updated on ", Sys.Date(), "."))
-message(paste0("Website last updated ", max(mosquito_data_latest$count_date), "."))
-
-write.csv(master_data, "mosquito_data.csv", row.names = FALSE)
-
-message("Data saved to repository.")
-
-
+  return(data)
 }
 
+master_data_old <- read_csv(url("https://github.com/colebaril/Mosquito_Monitor/blob/main/mosquito_data.csv?raw=TRUE")) |> 
+  filter(!date %in% c("2026-05-13", "2026-05-11")) |> 
+  select(-2:-3)
 
-
+if(max(master_data_old$date) == date_updated) {
+  
+  message(paste0("Data is already up to date as of ", Sys.Date(), "."))
+  message(paste0("Website last updated: ", date_updated, "."))
+  
+} else if(date_updated > max(master_data_old$date)) {
+  
+  data <- scrape_and_save_data()
+  
+  trap_data_metro <- data %>%
+    row_to_names(24) %>% 
+    select(matches("[a-z][a-z]")) %>% 
+    select(-1) %>% slice(1) %>% 
+    pivot_longer(cols = matches("[a-z][a-z]"),
+                 names_to = "trap",
+                 values_to = "number") %>% 
+    mutate(number = na_if(number, "**")) %>% 
+    mutate(number = na_if(number, "*")) %>% 
+    mutate(date = date_updated) %>% 
+    mutate(region = "Out of City Limits") %>% 
+    mutate(number = as.numeric(number)) %>% 
+    distinct(trap, .keep_all = TRUE) %>% 
+    mutate(region_name = case_when(str_detect(trap, "aa") ~ "Lilyfield",
+                                   str_detect(trap, "bb") ~ "North Perimeter",
+                                   str_detect(trap, "cc") ~ "West St. Paul",
+                                   str_detect(trap, "dd") ~ "East St. Paul",
+                                   str_detect(trap, "ee") ~ "Springfield",
+                                   str_detect(trap, "ff") ~ "Ritchot",
+                                   str_detect(trap, "gg") ~ "MacDonald",
+                                   str_detect(trap, "hh") ~ "Headingly",
+                                   str_detect(trap, "ii") ~ "Rosser",
+                                   TRUE ~ NA)) 
+  
+  trap_data_wpg <- data %>% 
+    select(1, 2) %>% 
+    rename(trap = X1,
+           number = X2) %>% 
+    filter(str_detect(trap, "^[A-Za-z][A-Za-z]\\d$")) %>% 
+    mutate(date = as.Date(date_updated, format = "%Y-%m-%d")) %>%
+    mutate(region = str_extract(trap, "^[A-Za-z][A-Za-z]")) %>% 
+    mutate(number = na_if(number, "**")) %>% 
+    mutate(number = na_if(number, "*")) %>% 
+    mutate(number = as.numeric(number)) %>% 
+    distinct(trap, .keep_all = TRUE) %>% 
+    mutate(region_name = trap)
+  
+  master_data <- rbind(trap_data_wpg, trap_data_metro, master_data_old) 
+  
+  message(paste0("Data has been updated on ", Sys.Date(), "."))
+  message(paste0("Website last updated ", date_updated, "."))
+  
+  write.csv(master_data, "mosquito_data.csv", row.names = FALSE)
+}
 
